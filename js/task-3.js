f80c5b0f12eb5b540507d32b40d4e4d9964c4fc8
george9907 <109024372+george9907@users.noreply.github.com>
2023-12-17 20:30:02 +0100

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,11 @@
 function getElementWidth(content, padding, border) {
-  const contentWidth = parseFloat(content);
-  const paddingWidth = parseFloat(padding);
-  const borderWidth = parseFloat(border);
+  const contentWidth = Number.parseFloat(content);
+  const paddingWidth = Number.parseFloat(padding);
+  const borderWidth = Number.parseFloat(border);
     
     const totalWidth = contentWidth + paddingWidth * 2 + borderWidth * 2;
 
-    return (`${totalWidth}px`);
+    return (totalWidth);
 }
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
